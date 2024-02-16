547c6b539b0119a4c9ad675690af38f058aeaa6b
c3sar-0 <aa4549807@gmail.com>
2024-02-16 02:04:04 -0500

    implemented car model and make

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -31,9 +31,11 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'localhost',
     'https://aa4549807-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'https://aa4549807-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/'
 ]
 CSRF_TRUSTED_ORIGINS = [
-    'https://aa4549807-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'
+    'https://aa4549807-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'https://aa4549807-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
 
 REST_FRAMEWORK = {
